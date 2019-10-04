42639ef6c4a5553d83133815ee0428e5ffb269c3
ishita sharma <ishitasharma12699@gmail.com>
2019-10-04 15:48:40 +0530

    Search with RE

--- reg_exp.py
+++ reg_exp.py
@@ -1,2 +1,9 @@
 print("Hello world!")
 print("Let's start with regular expressions...")
+
+import re
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if re.search('From:',line):
+        print("Found",line)
